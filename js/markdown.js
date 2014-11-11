d32f4a3b7af59f3ebbbca27b8fe9cd3c168dda12
spbreed <rockarthik@live.in>
2014-11-10 23:58:29 -0500

    test

--- js/markdown.js
+++ js/markdown.js
@@ -125,6 +125,8 @@
 
 		options = getSlidifyOptions( options );
 
+		markdown = markdown.replace(/(\r\n|\r)/g, '\n');
+		
 		var separatorRegex = new RegExp( options.separator + ( options.verticalSeparator ? '|' + options.verticalSeparator : '' ), 'mg' ),
 			horizontalSeparatorRegex = new RegExp( options.separator );
 
